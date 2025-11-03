1d264fa8e0485b0f154a7267b4478662e12cfd25
Manish3451 <manishmandal10@hotmail.com>
2025-11-03 22:20:26 +0530

    three sum added

---
+++ DSA/ararys/three_dsa.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n - 2):
+            # skip duplicate values for the fixed i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+
+                if s == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    # move left and right past duplicates
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < 0:
+                    # sum too small -> increase left to get a bigger number
+                    left += 1
+                else:
+                    # sum too large -> decrease right to get a smaller number
+                    right -= 1
+
+        return res
